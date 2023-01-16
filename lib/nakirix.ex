3776c365139f298c6cbdcfd49bf788b209622f7e
Eddy ruiz Azcuy <eddyraz.fl@gmail.com>
2023-01-16 11:13:33 -0500

    Initial Load

---
+++ lib/nakirix.ex
@@ -0,0 +1,18 @@
+defmodule Nakirix do
+  @moduledoc """
+  Documentation for `Nakirix`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Nakirix.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/nakirix_test.exs
@@ -0,0 +1,8 @@
+defmodule NakirixTest do
+  use ExUnit.Case
+  doctest Nakirix
+
+  test "greets the world" do
+    assert Nakirix.hello() == :world
+  end
+end
